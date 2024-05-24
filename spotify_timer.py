ab534321973a379bb18e6bb35f11e3a7c8554c87
Arbab Ansari <112172768+ArbAnk007@users.noreply.github.com>
2024-05-24 11:00:11 +0530

    Update spotify_timer.py

--- spotify_timer.py
+++ spotify_timer.py
@@ -1,10 +1,31 @@
 import time, os
 
-pid = int(input('Enter PID: '))
-end_time = int(input('Enter time in minutes: '))*60
+def remove_blank(str):
+    if str == "":
+        return False
+    return True
 
-for a in range(end_time):
-    time.sleep(1)
-    print(end_time-a)
+all_programs = os.popen("wmic process get description, processid").read()
 
-os.kill(pid, 9)
+all_programs_arr = list(filter(remove_blank, all_programs.split("\n")))
+
+required_program = None
+for program in all_programs_arr:
+    if "Spotify.exe" in program:
+        required_program = program
+        break
+
+if required_program != None:
+    try:
+        required_pid = int(list(filter(remove_blank, required_program.split(" ")))[1])
+        end_time = int(input('Enter time in minutes: '))*60
+        for a in range(end_time):
+            time.sleep(1)
+            print(end_time-a)
+        os.kill(required_pid, 9)
+    except:
+        print("Something went wrong!")
+        time.sleep(3)
+else:
+    print("Spotify not running!")
+    time.sleep(3)
